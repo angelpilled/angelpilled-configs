fb359cbaaa0ac3de1dbf8fc0bdf8e2a4ceaea274
angelpilled <angelpilleddev@gmail.com>
2023-10-28 13:21:24 +0200

    Added ChatGPT plugin and other plugins such as telescope, nui, popup and plenary

--- neovim/init.lua
+++ neovim/init.lua
@@ -26,6 +26,12 @@ Plug 'https://github.com/neoclide/coc.nvim'  " Auto Completion
 Plug 'https://github.com/neoclide/coc-pairs'
 Plug 'https://github.com/numToStr/Comment.nvim' " Neovim Commenting
 Plug 'https://github.com/ziontee113/color-picker.nvim'
+Plug 'https://github.com/nvim-lua/plenary.nvim'
+Plug 'https://github.com/jackMort/ChatGPT.nvim'
+Plug 'https://github.com/nvim-telescope/telescope.nvim'
+Plug 'https://github.com/xiyaowong/telescope-emoji.nvim'
+Plug 'https://github.com/MunifTanjim/nui.nvim'
+Plug 'https://github.com/nvim-lua/popup.nvim'
 
 call plug#end()
 ]]
@@ -34,7 +40,7 @@ call plug#end()
 require('Comment').setup()
 
 require("color-picker").setup({ -- for changing icons & mappings
-	["icons"] = { "■", "" },
+	["icons"] = { "■", "🐧" },
 	["border"] = "rounded", -- none | single | double | rounded | solid | shadow
 	["keymap"] = { -- mapping example:
 		["u"] = "<Plug>ColorPickerSlider5Decrease",
@@ -43,6 +49,12 @@ require("color-picker").setup({ -- for changing icons & mappings
 	["background_highlight_group"] = "Normal", -- default
 	["border_highlight_group"] = "FloatBorder", -- default
   ["text_highlight_group"] = "Normal", --default
+})
+
+require("telescope").load_extension("emoji")
+
+require("chatgpt").setup({
+    api_key_cmd = "op read op://private/OpenAI/credential --no-newline"
 })
 
 -- Execute commands on startup
@@ -63,9 +75,12 @@ vim.api.nvim_set_keymap('n', '<C-w>', ':-', { noremap = true})
 vim.api.nvim_set_keymap('n', '<C-e>', ':+', { noremap = true})
 vim.api.nvim_set_keymap('n', '<C-s>', ':w!<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-d>', ':q!<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<C-c>', ':PickColor<CR>', { noremap = true, silent = true })
-
-
+vim.api.nvim_set_keymap('n', '<C-c>', ':PickColor<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<C-g>', ':Telescope find_files<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<C-v>', ':Telescope emoji<CR>', { noremap = true})
+vim.api.nvim_set_keymap('n', '<C-p>', '"*p', { noremap = true})
+vim.api.nvim_set_keymap('n', '<A-q>', ':ChatGPT<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<A-w>', ':ChatGPTActAs<CR>', { noremap = true, silent = true})
 
 -- Autocommands - setting nowrap on buffer enter
 vim.api.nvim_create_autocmd('BufEnter', {
