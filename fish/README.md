d3127a0f36d5a6c016e50e1a383f392d994a6387
angelpilled <angelpilleddev@gmail.com>
2023-12-16 21:25:32 +0100

    scd script and fish directory added

---
+++ fish/README.md
@@ -0,0 +1,4 @@
+# fish shell
+Copy config.fish to $USER/.config/fish/config.fish
+
+I personally use `Tomorrow Night Bright` as fish theme, you can select this in the fish webui after executing `fish_config` inside fish.
---
+++ fish/config.fish
@@ -0,0 +1,35 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_prompt
+	set_color FF5FD7; echo -n (whoami);
+	set_color FFFFFF; echo -n ' at ';
+	set_color 00AFFA; echo -n (uname -n) "in";
+	set_color FFD7AF; echo -n " "; echo (pwd | sed "s|^$HOME|~|");
+	set_color FFFFFF; echo '$ '
+end
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+# Beautifying commands
+alias neofetch='neofetch | dotacat'
+alias cow='fortune | cowsay'
+alias startx='startx > /dev/null 2>&1'
+
+# Utilities
+alias cpc='xclip -sel c <'
+alias c='clear'
+alias xampp='sudo /opt/lampp/manager-linux-x64.run'
+
+# "Short"cuts
+alias nv='nvim'
+alias yt-dlp-mp3='yt-dlp -x --audio-format mp3'
+alias yt-dlp-mp4='yt-dlp -x --format mp4 -k'
+
+# Fun commands
+alias bad-apple='cvlc -V aa -q --no-video-title-show bad-apple.mp4'
+
+# Typos
+alias gti='git'
