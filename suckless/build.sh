fd395842cfa179495b77ba146ed395f8c7416d41
angelpilled <angelpilleddev@gmail.com>
2023-12-20 20:31:07 +0100

    Modified the build script, now it is fully functional

--- suckless/build.sh
+++ suckless/build.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
-
 # This shell script will build all the suckless files I use.
-# dwm, dmenu, st, slstatus
 
-cd ./dwm/
-sudo make clean install
+echo -e "Dependencies will be installed if they are needed...\n"
+sudo pacman -S --needed --noconfirm make gcc libavif gcr webkit2gtk
+yay -S --needed --noconfirm ttf-iosevka ttf-font-awesome
 
-cd ../st 
-sudo make clean install
+echo -e
+read -p "Will you be using suckless software on PC or laptop? (pc/laptop) " platform
+platform=${platform,,}
 
-cd ../dmenu
-sudo make clean install
+if [ $platform == "laptop" ]; then
+    sudo cp ./dwm/config.h.x260 ./dwm/config.h
+    sudo cp ./st/config.h.x260 ./st/config.h
+    sudo cp ./dmenu/config.h.x260 ./dmenu/config.h
+    sudo cp ./slstatus/config.h.x260 ./slstatus/config.h
+elif [ $platform == "pc" ]; then
+    sudo cp ./dwm/config.h.pc ./dwm/config.h
+    sudo cp ./st/config.h.pc ./st/config.h
+    sudo cp ./dmenu/config.h.pc ./dmenu/config.h
+    sudo cp ./slstatus/config.h.pc ./slstatus/config.h
+    sudo cp ./surf/config.h.pc ./surf/config.h
+    sudo cp ./slock/config.h.pc ./slock/config.h
+else
+    echo "The what?"
+    exit
+fi
 
-cd ../slstatus
-sudo make clean install
+echo -e "Suckless software is being installed on a ${platform~~}.\n"
 
-cd ../surf
-sudo make clean install
+cd ./dwm/ && sudo make clean install
+cd ../st && sudo make clean install
+cd ../dmenu && sudo make clean install
+cd ../slstatus && sudo make clean install
+cd ../surf && sudo make clean install
+cd ../slock && sudo make clean install
 
-cd ../slock
-sudo make clean install
+echo -e "\nSuccess!\nEverything should be up and running. Append \"exec dwm\" on the last line of your .xinitrc file in the home directory."
